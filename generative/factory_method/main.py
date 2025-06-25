bf5af93ea0116c85607b0030e295dd00dbed6a0d
irsCooper <spock187sheldon@gmail.com>
2025-06-25 15:32:19 +0400

    factory method fix client

--- generative/factory_method/main.py
+++ generative/factory_method/main.py
@@ -1,15 +1,15 @@
 from creator import BoatCreator, CarCreator, Creator
 
 
-def example(creator: Creator) -> None:
+def client(creator: Creator) -> None:
     print(f"Client: I'm not aware of the creator's class, but it still works.\n"
           f"{creator.some_operation()}", end="")
     
 
 if __name__ == "__main__":
     print("App: Launched with the ConcreteCreator1.")
-    example(CarCreator())
+    client(CarCreator())
     print("\n")
 
     print("App: Launched with the ConcreteCreator2.")
-    example(BoatCreator())
+    client(BoatCreator())
